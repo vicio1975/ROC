fb19b487b40f3927f9482c91a0552dc895a1a807
Vicio1975 <v.sammartano@gmail.com>
2018-07-13 18:14:32 +0100

    step1

--- roc.py
+++ roc.py
@@ -4,12 +4,15 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 fileName = "success_FR.txt"
 f = open(fileName,"r")
 ID = []
 FR = []
-ll = []
+fpi = []
 
 f = open(fileName,"r+")
 next(f)
@@ -18,10 +21,37 @@ for line in f:
     ID.append(float(line[0]))
     FR.append(float(line[1]))    
 
-fpi = []
+FRmax = max(FR) #max Frequency Ratio
+fp_ind = [(100*x)/FRmax for x in FR] #flood probablity index (%)
+fp_ind_sort = np.sort(fp_ind)        #flood probablity index sorted
 
-FRmax = max(FR); #max Frequency Ratio
-print(FRmax)
+T = len(fp_ind_sort)
+N = 0
+for i in fp_ind_sort:
+    if i <=50:
+        N += 1
+P = T-N
+print("Events: {}".format(T))
+print("Positives: {}".format(P))
+print("Negatives: {}".format(N))
+        
+x = np.linspace(min(fp_ind),max(fp_ind), 100)
+roc_x = []
+roc_y = []
 
-fpi = [(100*x)/FRmax for x in FR] #flood probablity index (%)
+TP = 0
+FN = 0
 
+for (i,j) in enumerate(x):
+    for i in range(0,len(fp_ind_sort)):
+        if (fp_ind_sort[i] < j) and (j<=50):
+            FN += 1
+        elif (fp_ind_sort[i] < j) and (j>50):
+            TP += 1
+    roc_x.append(FN/float(N))
+    roc_y.append(TP/len(fp_ind_sort))
+    FN = 0
+    TP = 0
+    
+plt.plot(x,roc_y)
+plt.show()
